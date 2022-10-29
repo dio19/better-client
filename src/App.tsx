8668f66ec36129478cdf5b2dc44b9191ffdd31cb
diomeli19 <dionisio.hernandez@mercadolibre.com>
2022-10-29 20:58:08 -0300

    feat: routes app were added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import {
+    BrowserRouter ,
+    Routes,
+    Route,
+} from 'react-router-dom';
 import Navbar from './components/NavBar';
-  
+
 import './App.scss';
 
-function App() {
+const App = () => {
     return (
+        <BrowserRouter>
         <div className="App">
         <Navbar/>
+        <Routes>
+            <Route path="/" element={<h3>Home</h3>}  />
+            <Route path="/customers" element={<h3>Customers Table</h3>} />
+            <Route path="/add-customer" element={<h3>Post</h3>} />
+            <Route path="/update-or-delete" element={<h3>Put/Delete</h3>} />
+            {/* <Route path="/customers/:id" element={<<h3>GetByID</h3>} /> */}
+        </Routes>
         </div>
+    </BrowserRouter>
     );
 }
 
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
+import {
+    NavLink
+  } from 'react-router-dom';
 import './index.scss';
 
 const NavBar = () => {
     return (
         <nav className="navbar">
-            <a className="navbar__title" href="/" >{<h1>Customers App</h1>}</a>
+            <NavLink className="navbar__title" to="/" children={<h1>Customers App</h1>}></NavLink>
                 <ul className="navbar__list">
-                    <a
-                        href="/"
-                        >{<li className="navbar__list-item"><div className="navbar__list-item-link">Home</div></li>}
-                    </a>
-                    <a
-                        href="/customers"
-                        >{<li className="navbar__list-item"><div className="navbar__list-item-link">Customers</div></li>}
-                    </a>
+                    <NavLink
+                        to="/"
+                        children={<li className="navbar__list-item"><div className="navbar__list-item-link">Home</div></li>}
+                    ></NavLink>
+                    <NavLink
+                        to="/customers"
+                        children={<li className="navbar__list-item"><div className="navbar__list-item-link">Customers</div></li>}
+                    ></NavLink>
                 </ul>
         </nav>
     )
 }
 
 export default NavBar;
+
